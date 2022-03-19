d7221faf36c89a6b6f91e943ce3abd77d78d24d7
Iddrisu Salifu <101422404+Bagadii@users.noreply.github.com>
2022-03-19 11:02:42 +0000

    Update sQL_case_statement.sql

--- sQL_case_statement.sql
+++ sQL_case_statement.sql
@@ -1,26 +1,26 @@
 ----This is to find if customers year meets a specific condition
 ----ie if the year is greater than 2015 then get the age if not give nothing
-SLECT
+SELECT
 ----this gets the year
-lastart.year,
+  lastart.year,
 ----this gets the customer age
-labstart.cuctomer_age,
+  labstart.cuctomer_age,
 ---this case statement analyse is condition is met
-CASE WHEN lastart.year >2015
-THEN labstatrt.customer_age
-ELSE NULL
-END AS age_analyze
+  CASE WHEN lastart.year >2015
+     THEN labstatrt.customer_age
+     ELSE NULL
+   END AS age_analyze
 FROM lastar
 
 ----Counts based on condition
-SLECT
+SELECT
 ---this gets the year
 lastart.year,
 ----thirt.cuctomer_age,
 -----this case statement analyse is condition is met
-CASE WHEN lastart.year >2011
-THEN count(labstatrt.customer_age)
-ELSE NULL
-END AS number_0f_customer
+   CASE WHEN lastart.year >2011
+      THEN count(labstatrt.customer_age)
+      ELSE NULL
+  END AS number_0f_customer
 FROM lastart
-Group by labstart-year
+  Group by labstart-year
