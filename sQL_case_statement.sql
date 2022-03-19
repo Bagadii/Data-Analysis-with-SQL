00e75bded1cff9158922d76c006bdfda712dfa7a
Iddrisu Salifu <101422404+Bagadii@users.noreply.github.com>
2022-03-19 10:45:43 +0000

    Add files via upload

---
+++ sQL_case_statement.sql
@@ -0,0 +1,13 @@
+----This is to find if customers year meets a specific condition
+----ie if the year is greater than 2015 then get the age if not give nothing
+SLECT
+....this gets the year
+lastart.year,
+this gets the customer age
+labstart.cuctomer_age,
+this case statement analyse is condition is met
+CASE WHEN lastart.year >2015
+THEN labstatrt.customer_age
+ELSE NULL
+END AS age_analyze
+FROM lastart
